2a69d8f25193680754ed0361d7dd50c2f536057c
Harsh-Vaibhav <harshvaibhav2307@gmail.com>
2025-06-20 10:36:41 +0530

    updated

---
+++ JavaScript/file3.js
@@ -0,0 +1,40 @@
+// let a = 10
+// let b= 12
+// if (a>b){
+//     console.log(`${a} is greater`)
+// }
+// else{
+//     console.log(`${b} is greater`)
+// }   CURLY BRACKET IS NOT REQUIRED FOR A SINGLE LINE IF ELSE
+// if (a>b)
+//     console.log(`${a} is greater`)
+// else
+//     console.log(`${b} is greater`)
+
+// let c //undefined returns the else part
+//let c=null;
+// if(c) console.log("God Morning")
+// else console.log("Good Afternoon")
+
+// let v1=10;
+// let v2=10;
+// let v3=15;
+// if (v1 > v2 && v1 > v3) console.log("v1 is greater");
+// else if (v2 > v1 && v2 > v3) console.log("v2 is greater");
+// else console.log("v3 is greater")
+
+// let x=5;
+// let y=2;
+// x > y ? console.log("x is greater") : console.log("y is greater")
+// x > y && console.log("x is greater"); // returns only if the consition is true, if false then no return
+
+// let a
+// let value = a||20
+// console.log(value)
+// let b=10
+// let value1=b||30
+// console.log(value1)
+
+let a = 10
+let value = a??11
+console.log
