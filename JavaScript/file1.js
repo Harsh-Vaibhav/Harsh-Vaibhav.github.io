c167e6bf3d7530a6e068ffa327342e670437786f
Harsh-Vaibhav <harshvaibhav2307@gmail.com>
2025-06-20 10:04:18 +0530

    updated

---
+++ JavaScript/file1.js
@@ -0,0 +1,25 @@
+// console.log("hello World")
+let Studentname="John"
+let age=21
+let promoted=true
+let score
+let city=null
+let country="India"
+
+console.log(Studentname)
+console.log(typeof(Studentname))
+console.log(typeof(score))
+console.log(typeof city)
+console.log(typeof age)
+let strAge=String(age)
+console.log(typeof strAge)
+
+let rating="5"
+console.log(typeof rating)
+let strRating = Number(rating)
+console.log(typeof strRating)
+
+let flag="true"
+console.log(typeof flag)
+let bFlag= Boolean(flag)
+console.log(typeof bFlag)
---
+++ JavaScript/file2.js
@@ -0,0 +1,23 @@
+// let a=54;
+// let b=10;
+// console.log(a+b)
+// console.log(a%b)
+// let a="10"
+// let b= 10
+// console.log(a+b)
+// console.log(a*b)
+// console.log(a==b)
+// console.log(a===b)
+// console.log(a!==b)
+// console.log(a!=b)
+
+// let a=10
+// let b=2
+// let c=12
+// console.log(a>b && a>c)
+// console.log(a>b || a>c)
+
+let a=10
+let b=2
+console.log(!a>b)
+console.log(a**b)
